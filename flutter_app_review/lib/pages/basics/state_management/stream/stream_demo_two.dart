86121edfde93df0feb05b56266970e67806f7434
ian_kevin126 <ian_kevin126@126.com>
2020-04-01 19:32:41 +0800

    Stream的使用

--- flutter_app_review/lib/pages/basics/state_management/stream/stream_demo_two.dart
+++ flutter_app_review/lib/pages/basics/state_management/stream/stream_demo_two.dart
@@ -9,6 +9,9 @@ class StreamDemoTwo extends StatefulWidget {
 
 class _StreamDemoTwoState extends State<StreamDemoTwo> {
   StreamSubscription _streamSubscription;
+  StreamController _streamDemo;
+  StreamSink _sinkDemo;
+  String _data = '...';
 
   @override
   void initState() {
@@ -16,11 +19,27 @@ class _StreamDemoTwoState extends State<StreamDemoTwo> {
 
     // 初始化Stream
     print('====initStream=====');
-    Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    // Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    // 创建单次订阅的Stream
+    // _streamDemo = StreamController<String>();
+
+    // 创建可以多次订阅的Stream
+    _streamDemo = StreamController.broadcast();
+
+    // 给Stream增加数据的方法sink
+    _sinkDemo = _streamDemo.sink;
 
     // 监听stream，会返回一个StreamSubscription的实例
     print('====listenStream=====');
-    _streamSubscription = _streamDemo.listen(onData, onError: onError, onDone: onDone);
+    _streamSubscription = _streamDemo.stream.listen(onData, onError: onError, onDone: onDone);
+    // 添加第二次订阅
+    _streamDemo.stream.listen(onDataTwo, onError: onError, onDone: onDone);
+  }
+
+  @override
+  void dispose() {
+    _streamDemo.close();
+    super.dispose();
   }
 
   void onError(error) {
@@ -31,15 +50,22 @@ class _StreamDemoTwoState extends State<StreamDemoTwo> {
     print('======done!===');
   }
 
-  void onData(String data) {
-    print('====data=====$data');
+  void onData(dynamic data) {
+    print('====onDataOne=====$data');
+    setState(() {
+      _data = data;
+    });
+  }
+
+  void onDataTwo(dynamic data) {
+    print('====onDataTwo=====$data');
   }
 
   // 模拟获取数据
   Future<String> fetchData() async {
     await Future.delayed(Duration(seconds: 3));
-    throw 'Something happened!'; // 模拟异常
-//    return 'hello~';
+//    throw 'Something happened!'; // 模拟异常
+    return 'hello~';
   }
 
   // 暂停Stream，之后是可以恢复的
@@ -60,6 +86,13 @@ class _StreamDemoTwoState extends State<StreamDemoTwo> {
     _streamSubscription.cancel();
   }
 
+  void _addDataToStream() async {
+    print('====add data to stream====');
+    String data = await fetchData();
+//     _streamDemo.add(data);
+    _sinkDemo.add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,6 +105,22 @@ class _StreamDemoTwoState extends State<StreamDemoTwo> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // 这种是原来的获取值的方式，需要setState方法去刷新页面
+            Text('Stream值：$_data'),
+            StreamBuilder(
+              stream: _streamDemo.stream,
+              initialData: '...',
+              builder: (context, snapshot) {
+                return Text('${snapshot.data}');
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            RaisedButton(
+              child: Text('添加事件到Stream'),
+              onPressed: _addDataToStream,
+            ),
             RaisedButton(
               child: Text('暂停Stream'),
               onPressed: _pauseStream,
